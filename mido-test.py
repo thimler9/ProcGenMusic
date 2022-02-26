596322bd0d3b6509f2cd8104172931c6820ebe7b
thimler9 <thimler9@vt.edu>
2022-02-26 10:57:53 -0500

    Beats are working

--- mido-test.py
+++ mido-test.py
@@ -22,9 +22,9 @@ whole_tone = [36, 38, 40, 42, 44, 46] # ^ but for whole tone scale
 def get_note(note_type, ticks_per_beat, time_sig_denominator):
     return int(note_type.value * (time_sig_denominator / 4) * ticks_per_beat)
 
-def append_to_track(note_pitch, velocity, start_time, note_type, ticks_per_beat, time_sig_denominator, track):
+def append_to_track(note_pitch, velocity, note_type, ticks_per_beat, time_sig_denominator, track):
     end_time = get_note(note_type, ticks_per_beat, time_sig_denominator)
-    track.append(Message('note_on', note=note_pitch, velocity=velocity, time=start_time))
+    track.append(Message('note_on', note=note_pitch, velocity=velocity, time=0))
     track.append(Message('note_off', note=note_pitch, velocity=velocity, time=end_time))
     return end_time
 
@@ -52,33 +52,39 @@ def create_music(time_duration, key_signature, time_sig_numerator, time_sig_deno
     beats_left_in_measure = float(time_sig_numerator)
     # Generates a note per iteration
     while mid.length < time_duration:
-        curr_note = int((opensimplex.noise2(mid.length * 2, 1) + 1) / 2 * (96 - 36) +  36)
+        curr_note = int((opensimplex.noise2(mid.length * 2, 1) + 1) / 2 * 60 +  36)
         curr_note_type = get_note_type(beats_left_in_measure, mid.length)
         beats_left_in_measure -= curr_note_type.value
         if beats_left_in_measure <= 0:
             beats_left_in_measure = float(time_sig_numerator)
-        append_to_track(get_note_in_scale(curr_note, scale), 64, 0, curr_note_type, mid.ticks_per_beat, time_sig_denominator, track)
+        append_to_track(get_note_in_scale(curr_note, scale), 64, curr_note_type, mid.ticks_per_beat, time_sig_denominator, track)
 
-def create_rhythm(bar_duration, key_signature, time_sig_numerator, time_sig_denominator, scale, track, mid):
+def generate_measure(time_sig_numerator, measure_number):
     beats_left_in_measure = float(time_sig_numerator)
-    velocity = 64
-    notes = []
-    i = 0;
+    measure = []
 
-    # Generates a note per iteration
-    while i < bar_duration:
-        curr_note = int((opensimplex.noise2(mid.length * 2, 1) + 1) / 2 * (96 - 36) +  36)
+    while beats_left_in_measure > 0:
+        curr_note = int((opensimplex.noise2(beats_left_in_measure, measure_number * 100) + 1) / 2 * 60 +  36)
         curr_note_type = get_note_type(beats_left_in_measure, mid.length)
-        beats_left_in_measure -= curr_note_type.value
-        if beats_left_in_measure <= 0:
-            beats_left_in_measure = float(time_sig_numerator)
-            velocity = int((opensimplex.noise2(mid.length * 2, 10) + 1) / 2 * (104 - 84) +  64)
+        velocity = 0
+        if beats_left_in_measure == float(time_sig_numerator):
+            velocity = int((opensimplex.noise2(beats_left_in_measure, measure_number * 1000) + 1) / 2 * 10 + 75)
         else:
-            velocity = int((opensimplex.noise2(mid.length * 2, 10) + 1) / 2 * (74 - 54) +  64)
-        i += 1
-        notes[i] = (get_note_in_scale(curr_note, scale), velocity, curr_note_type)
+            velocity = int((opensimplex.noise2(beats_left_in_measure, measure_number * 2000) + 1) / 2 * 30 + 40)
+        beats_left_in_measure -= curr_note_type.value
+        measure.append((curr_note, velocity, curr_note_type))
+    return measure
+
+def create_rhythm(measure_count, time_duration, key_signature, time_sig_numerator, time_sig_denominator, scale, track, mid):
+    measures = []
+    for i in range(measure_count):
+        measures.append(generate_measure(time_sig_numerator, i))
     
-    return notes
+    while mid.length < time_duration:
+        for measure in measures:
+            for note in measure:
+                (note_pitch, velocity, note_type) = note
+                append_to_track(note_pitch, velocity, note_type, mid.ticks_per_beat, time_sig_denominator, track)
 
 def create_midi(bpm, scale, time_sig_numerator, time_sig_denominator, seed):
     opensimplex.seed(seed)
@@ -99,12 +105,11 @@ def create_midi(bpm, scale, time_sig_numerator, time_sig_denominator, seed):
     time_duration = 20
 
     create_music(time_duration, 0, time_sig_numerator, time_sig_denominator, scale, melody, mid)
-    create_rhythm(8, 0, time_sig_numerator, time_sig_denominator, scale, bass, mid)
-
-    while mid.length < time_duration:
-        append_to_track(get_note_in_scale(curr_note, scale), velocity, 0, curr_note_type, mid.ticks_per_beat, time_sig_denominator, track)
+    create_rhythm(2, time_duration, 0, time_sig_numerator, time_sig_denominator, scale, bass, mid)
 
     mid.save('./midi-gen/' + str(seed) + '.mid')
+
+    print(len(mid.tracks))
     return mid
 
 
